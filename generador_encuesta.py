c4467fd0960f7c8d2f2414994e49700075ca1081
ZakuG <lcastrob2@correo.uss.cl>
2025-03-28 17:57:32 -0300

    first commit

---
+++ generador_encuesta.py
@@ -0,0 +1,21 @@
+import random
+
+class CreateRespuesta:
+    def __init__(self, Sexo, Edad, Respuesta):
+        self.Sexo = Sexo
+        self.Edad = Edad
+        self.Respuesta = Respuesta
+    
+    def funcion(self):
+        return f"{self.Sexo} {self.Edad} {self.Respuesta}\n"
+
+def createCSV():
+    with open("Solemne_1\encuesta_habitos.csv", "w") as archivo:
+        archivo.write("Sexo Edad Respuesta\n")
+        for _ in range(300):
+            persona = CreateRespuesta("Femenino" if random.random()<=0.5 else "Masculino", random.randint(18,45), "Sí" if random.randint(1,3)==1 else "No" if random.randint(2,3)==2 else "Tal vez")
+            archivo.write(persona.funcion())
+
+        archivo.close()
+
+createCSV()
